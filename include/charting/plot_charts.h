cd44094babdf7d9975cf717fbe3bcefb9b307b45
krishna <chaitanyaradon89@gmail.com>
2019-09-15 00:50:12 -0600

    Added Gnuplot istream

---
+++ include/charting/plot_charts.h
@@ -0,0 +1,8 @@
+//
+// Created by shivababa on 9/15/19.
+//
+
+#ifndef REMOTEFNIRS_PLOT_CHARTS_H
+#define REMOTEFNIRS_PLOT_CHARTS_H
+
+#endif //REMOTEFNIRS_PLOT_CHARTS_H
---
+++ src/charting/plot_charts.cpp
@@ -0,0 +1,4 @@
+//
+// Created by shivababa on 9/15/19.
+//
+
